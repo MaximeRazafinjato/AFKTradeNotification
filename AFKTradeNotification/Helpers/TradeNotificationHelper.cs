e4c7e1eb8b68d4972abf1212022fc8853c87ea0c
Maxime Razafinjato <maximerazafinjato@gmail.com>
2024-08-18 21:34:52 +0200

    ♻ Refactored the code to split the logic

--- AFKTradeNotification/Helpers/TradeNotificationHelper.cs
+++ AFKTradeNotification/Helpers/TradeNotificationHelper.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AFKTradeNotification.Helpers;
-
+﻿namespace AFKTradeNotification.Helpers;
 public class TradeNotificationHelper
 {
     public const string defaultUser = "Unknown sender";
